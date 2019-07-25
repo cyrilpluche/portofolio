760b04bdf6884075cf3e1b8379565fbc712e8461
cyrilpluche <cyril.pluche@etu.umontpellier.fr>
2019-07-25 14:59:24 +0200

    about me content

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,13 +1,44 @@
 import React, {useState} from 'react'
 import "./AboutMe.scss"
+import {lifeSteps} from "../data/AboutMe";
 
 const Timeline = ({step}) => {
 
     return (
         <div className="timeline-container">
-            <div className="timeline"></div>
+            <div className="timeline"/>
             <div className={["cursor", "step-" + step].join(" ")}>
-            <i className="material-icons">keyboard_arrow_left</i>
+                <i className="material-icons cursor-icon">keyboard_arrow_left</i>
+            </div>
+        </div>
+    )
+}
+
+const TimelineContent = ({activeItem, setHover}) => {
+
+    console.log(activeItem)
+
+    const isActive = item => {
+        console.log(activeItem)
+        return activeItem.date === item.date
+    }
+
+    return (
+        <div className="timeline-content-container">
+            <div className="container-row justify-between">
+                {lifeSteps.map((step, index) => (
+                    <div className={["container-col timeline-section", isActive(step) ? "timeline-section-active" : null].join(" ")} key={index} onMouseOver={() => setHover(step)}>
+                        <div className="text-m mb-1">{step.date}</div>
+                        <div className="timeline-detail container-col">
+                            {step.events.map((event, index) => (
+                                <React.Fragment key={index}>
+                                    <div>{event.title}</div>
+                                    <div className="t-grey-light mb-1">{event.description}</div>
+                                </React.Fragment>
+                            ))}
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
@@ -16,15 +47,24 @@ const Timeline = ({step}) => {
 const AboutMe = ({hidden}) => {
 
     const [step, setStep] = useState(0)
+    const [activeItem, setActiveItem] = useState(lifeSteps[0])
 
     const increase = () => {
         setStep(step + 1)
     }
 
+    const handleHover = item => {
+        setActiveItem(item)
+        setStep(lifeSteps.indexOf(item))
+    }
+
+    console.log(activeItem)
+
     return (
         <div className={["aboutme-container", hidden ? "hide-content" : "show-content"].join(" ")}>
             <div onClick={increase}>cacahuete</div>
             <Timeline step={step}/>
+            <TimelineContent activeItem={activeItem} setHover={handleHover}/>
         </div>
     )
 }
---
+++ src/data/AboutMe.js
@@ -0,0 +1,55 @@
+import linkedin from '../public/icon_linkedin.png'
+import github from '../public/icon_github.png'
+
+export const lifeSteps = [
+    {
+        date: '2019',
+        events : [
+            {
+                date: 'December',
+                title: 'Graduate of Polytech',
+                description: 'Engineering degree in Computer Science & Management',
+                location: 'Montpellier, France'
+            }
+        ]
+    },
+    {
+        date: '2016',
+        events : [
+            {
+                date: 'September',
+                title: 'Started Polytech Engineering cursus',
+                description: 'Computer Science & Management',
+                location: 'Montpellier, France'
+            }
+        ]
+    },
+    {
+        date: '2014',
+        events : [
+            {
+                date: 'September',
+                title: 'Polytech Preparatory Classes',
+                description: 'Mathematics, Physics, Chemistry, Computer science',
+                location: 'Montpellier, France'
+            },
+            {
+                date: 'June',
+                title: 'Holder of a Scientist Baccalaureat ranked "good"',
+                description: 'Equivalent to GCE "A" levels in science passed with good grades',
+                location: 'Dijon, France'
+            }
+        ]
+    },
+    {
+        date: '2012',
+        events : [
+            {
+                date: 'September',
+                title: 'Football high level athlete',
+                description: 'Player of the 19 years old squad of Dijon F.C.O (Ligue 1)',
+                location: 'Dijon, France'
+            }
+        ]
+    },
+]
